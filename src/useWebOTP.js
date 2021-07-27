f8796ee3970e0735246f867f96935a58b23c2478
Farid Shokri <f.shokri@digikala.com>
2021-07-27 20:21:21 +0430

    new update

--- src/useWebOTP.js
+++ src/useWebOTP.js
@@ -1,21 +1,26 @@
 function useWebOTP() {
+  if ("OTPCredential" in window) {
+    const abortController = new AbortController();
+    const OTPValue = new Promise(function (resolve, reject) {
+      navigator?.credentials
+        ?.get({
+          otp: { transport: ["sms"] },
+          signal: abortController.signal,
+          mediation: "required",
+        })
+        .then((response) => {
+          document.querySelector("#id").value = response.code;
+          console.log(`response`, response);
+          resolve(response);
+        })
+        .catch((response) => {
+          console.log(`response`, response);
+          reject(response);
+        });
+    });
 
-  const abortController = new AbortController();
-  const OTPValue =  new Promise(function (resolve, reject) {
-    navigator?.credentials?.get({
-      otp: { transport: ['sms'] },
-      signal: abortController.signal,
-      mediation: "required",
-    }).then(response => {
-      console.log(`response`, response);
-      resolve(response);
-    }).catch(response => {
-      console.log(`response`, response);
-      reject(response);
-    })
-  });
-
-  return { abortController, OTPValue };
+    return { abortController, OTPValue };
+  }
 }
 
 export default useWebOTP;
